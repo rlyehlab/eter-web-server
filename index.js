c6b94a7a22f66a44c3189f1cda0ec10a31536d5a
Leo <leonardo.maddio@gmail.com>
2018-05-09 12:20:21 -0300

    Alert errors and add yarn

--- index.js
+++ index.js
@@ -4,17 +4,22 @@ const { postgraphile } = require("postgraphile");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(
-  postgraphile(process.env.DATABASE_URL),
-  process.env.SCHEMA,
-  {
-     watchPg: true,
-     graphiql: true,
-     dynamicJson: true,
-     disableDefaultMutations: true,
-     showErrorStack: true
-  }
-));
+if(!process.env.DATABASE_URL)
+    throw new Error("DATABASE_URL is needed!");
+if(!process.env.SCHEMA)
+    throw new Error("SCHEMA's name is needed!");
 
-app.listen(PORT);
+app.use(
+    postgraphile(process.env.DATABASE_URL),
+    process.env.SCHEMA,
+    {
+        watchPg: true,
+        graphiql: true,
+        dynamicJson: true,
+        disableDefaultMutations: true,
+        showErrorStack: true
+    }
+);
+
+app.listen(PORT, (a, b, c)=>console.log("a",a,"b",b,"c",c));
 console.log("PostGraphile listening in port: " + PORT);
