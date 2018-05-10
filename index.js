df308607130b148f930f9036beb3bbb5a1725008
Leonardo Maddio <leonardo.maddio@gmail.com>
2018-05-09 21:49:40 -0300

    Use enviroment variables

--- index.js
+++ index.js
@@ -2,24 +2,37 @@ const express = require("express");
 const { postgraphile } = require("postgraphile");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const env = {
+  "PORT": process.env.PORT || 3000,
+  "DATABASE_URL": process.env.DATABASE_URL,
+  "DB_NAME": process.env.DB_NAME,
+  "USER": process.env.USER,
+  "PASS": process.env.PASS,
+  "SCHEMA": process.env.SCHEMA
+};
 
-if(!process.env.DATABASE_URL)
-    throw new Error("DATABASE_URL is needed!");
-if(!process.env.SCHEMA)
-    throw new Error("SCHEMA's name is needed!");
+if(Object.entries(env).some(([k,v])=>{
+    console.log(k, v);
+    return !v;
+  })
+) {
+  throw new Error("One of the enviroment values wasn't provided");
+}
+
+const USER_PASS = [USER, PASS].join(":");
+const URL = "postgres://"+USER_PASS+DATABASE_URL+"/"+DB_NAME;
 
 app.use(
-    postgraphile(process.env.DATABASE_URL),
-    process.env.SCHEMA,
-    {
-        watchPg: true,
-        graphiql: true,
-        dynamicJson: true,
-        disableDefaultMutations: true,
-        showErrorStack: true
-    }
+  postgraphile(URL),
+  SCHEMA,
+  {
+    watchPg: true,
+    graphiql: true,
+    dynamicJson: true,
+    disableDefaultMutations: true,
+    showErrorStack: true
+  }
 );
 
-app.listen(PORT, (a, b, c)=>console.log("a",a,"b",b,"c",c));
+app.listen(PORT);
 console.log("PostGraphile listening in port: " + PORT);
